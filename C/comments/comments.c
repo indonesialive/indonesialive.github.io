fab14f29b4a9bed1ecd1b74f804ac5cd5f97ffb6
Amin Nur Muis <nusantaralive@outlook.com>
2026-01-13 21:29:23 +0700

    new practices

---
+++ C/comments/comments.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to the world of C!"); // this is single line comment
+    return 0;                             /*this is
+                                multi line comment*/
+}
---
+++ C/escapeSequence/escapeSequence.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is line 1\n");
+    printf("This is line 2\n");
+    printf("This is horizontal tab\thorizontal tab\n");
+    printf("This is backslash \\\n");
+    printf("This is double quote \"");
+    return 0;
+}
