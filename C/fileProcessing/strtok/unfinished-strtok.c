bf146964f7f021f65ca4758940d2aed07813d730
Amin Nur Muis <nusantaralive@outlook.com>
2026-02-06 06:26:32 +0700

    new practice

--- C/fileProcessing/strtok/unfinished-strtok.c
+++ C/fileProcessing/strtok/unfinished-strtok.c
@@ -6,6 +6,41 @@
 
 struct dataBuku
 {
-    char kode;
-    char nama;
+    char kode[6];
+    char nama[51];
+    char jenis[11];
+    int harga;
+};
+
+int main()
+{
+    FILE *fptr = fopen("databaseBuku.txt", "r");
+    if (fptr == NULL)
+    {
+        printf("failed to open file");
+        return 1;
+    }
+    struct dataBuku toko[max];
+    int count = 0;
+    char line[200];
+
+    while (fgets(line, sizeof(line), fptr) != NULL)
+    {
+        char *token;
+
+        token = strtok(line, "|");
+        strcpy(toko[count].kode, token);
+
+        token = strtok(NULL, "|");
+        strcpy(toko[count].nama, token);
+
+        token = strtok(NULL, "|");
+        strcpy(toko[count].jenis, token);
+
+        token = strtok(NULL, "|");
+        toko[count].harga = atoi(token);
+
+        printf("Kode=%s", toko[0].kode);
+    }
+    return 0;
 }
